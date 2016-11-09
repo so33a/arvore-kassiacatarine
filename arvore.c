1b234cc9327dde54974862bf11656a0a682dc268
Akira <kassia_catarine.15@hotmail.com>
2016-11-09 20:22:53 -0200

    Update arvore.c

--- arvore.c
+++ arvore.c
@@ -1,220 +1,151 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "arvore.c"
 #include "arvore.h"
-#include "fila.c"
 
-ARVORE novaArvore() {
-  ARVORE a = malloc(sizeof(struct arvore));//Aloca espaço para arvore
-  a->z = malloc(sizeof(struct node));
-  a->raiz = a->z;//Aponta para final
-  a->raiz->left = a->raiz->right = a->z;
-  a->z->left = a->z->right = a->z;
-  return a;
-}
+/*
+ *           55
+ *      44        66
+ *   33   45  64     77
+ *
+ *   em ordem = 33 44 45 55 64 66 77
+ *   pos ordem = 33 45 44 64 77 66 55
+ *   pre ordem = 55 44 33 45 66 64 77
+ *   em largura = 55 44 66 33 45 64 77
+ *
+ *
+ *   rotR (55) =>
+ *
+ *              44
+ *          33     55
+ *               45  66
+ *                  64 77
+ *
+ *   em largura: 44 33 55 45 66 64 77
+ *
+ */
+int main () {
+  ARVORE a = novaArvore();
+  link x;/*
+  inserir(a, 55);
+  inserir(a, 44);
+  inserir(a, 33);
+  inserir(a, 66);
+  inserir(a, 77);
+  inserir(a, 45);
+  inserir(a, 64);
+  printf ("pre: ");
+  imprimePreOrdem(a);
+  printf ("\n");
+  printf ("pos: ");
+  imprimePosOrdem(a);
+  printf ("\n");
+  printf ("em: ");
+  imprimeEmOrdem(a);
+  printf ("\n");
+  printf ("largura: ");
+  imprimeEmLargura(a);
+  printf ("\n");
 
-void imprimePreOrdemR(ARVORE a, link h) {//Sempre Imprime a direita
-  if (h != a->z) {
-    printf ("%d ", h->key);
-    imprimePreOrdemR(a, h->left);
-    imprimePreOrdemR(a, h->right);
-  }
-}
-
-int contaNosR (ARVORE a, link h) {
-  if(h == a->z) return 0;// Se for vazio
-  return 1 + contaNosR(a, h->left) + contaNosR(a, h->right);//
-}
-
-int contaParesR (ARVORE a, link h) {
-  if(h == a->z)
-    return 0;
-  if (h->key %2 == 0)
-    return 1 + contaParesR(a, h->left) + contaParesR(a, h->right);
+  x = busca(a, 45);
+  if (x == NULL)
+    printf ("Nao achou\n");
   else
-    return  contaParesR(a, h->left) + contaParesR(a, h->right);
-}
-
-void imprimePreOrdem (ARVORE a) {
-  imprimePreOrdemR(a, a->raiz);
-}
-void imprimePosOrdemR(ARVORE a, link h) {
-  if (h != a->z) {
-    imprimePosOrdemR(a, h->left);
-    imprimePosOrdemR(a, h->right);
-    printf ("%d ", h->key);
-  }
-}
-
-void imprimePosOrdem (ARVORE a) {
-  imprimePosOrdemR(a, a->raiz);
-}
+    printf ("Achou %d \n", x->key);
 
 
-void imprimeEmOrdemR (ARVORE a, link h) {
-   if (h != a->z) {
-     imprimeEmOrdemR(a, h->left);
-     printf("%d ", h->key);
-     imprimeEmOrdemR(a, h->right);
-   }
-}
-
-void imprimeEmOrdem (ARVORE a) {
-   imprimeEmOrdemR(a, a->raiz);
-}
-
-link buscaR (ARVORE a, link h, int key) {
-  if(h == a->z) return NULL;
-  if(h->key == key) return h;
-  if( h->key < key)
-    return buscaR(a, h->right, key);
-  return buscaR(a, h->left, key);
-}
-
-
-link busca (ARVORE a, int key){
-  return buscaR(a, a->raiz, key);
-}
-
-link novoNo(int key, link l, link r) {
-  link x = malloc(sizeof(struct node));
-  x->key = key;
-  x->left = l;
-  x->right = r;
-  return x;
-}
-link inserirR (ARVORE a, link h, int key) {
-  if(h == a->z)
-    return novoNo(key, a->z, a->z);
-  if(h->key == key) return h;
-  if(h->key < key)
-    h->right = inserirR(a, h->right, key);
+  x = busca(a, 1);
+  if (x == NULL)
+    printf ("Nao achou\n");
   else
-    h->left = inserirR(a, h->left, key);
-  return h;
-}
-link inserirT (ARVORE a, link h, int key) {
-  if(h == a->z)
-    return novoNo(key, a->z, a->z);
-  if(h->key == key) return h;
-  if(h->key < key)  {
-    h->right = inserirT(a, h->right, key);
-    h = rotL(a, h);
-  }
-  else {
-    h->left = inserirT(a, h->left, key);
-    h = rotR(a, h);
-  }
-  return h;
-}
-
-void inserirNaRaiz(ARVORE a, int key) {
-  a->raiz = inserirT(a, a->raiz, key);
-}
-
-void inserir (ARVORE a, int key){
-  a->raiz = inserirR(a, a->raiz, key);
-}
-
-void imprimeEmLargura (ARVORE a) {
-  link aux;
-  FILA f = novaFila();
-  enfilar(f, a->raiz);
-  while (f->primeiro != NULL) {
-    aux = desenfilar(f);
-    printf ("%d ", aux->key);
-    if (aux->left != a->z)  {
-      enfilar (f,aux->left);
-    }
-    if (aux->right != a->z) {
-      enfilar (f,aux->right);
-    }
-  }
-  destroiFila(f);
-}
-
-link rotL(ARVORE a, link h) {
-  link x = h->right;
-  h->right = x->left;
-  x->left = h;
-  return x;
-}
-link rotR(ARVORE a, link h) {
-  link x = h->left;
-  h->left = x->right;
-  x->right = h;
-  return x;
-}
-void remover (ARVORE a, int key){
-    removerNo (a,busca(a,key));
-}
-void removerNo (ARVORE a, link node){
-    link no = a->raiz;
-    link aux = node;
-    link pai = node;
-
-    if(node->right != a->z && node->left != a->z){//Possuir 2 filhos
-        node = node->right;//Vai para direita
-        if(node->left == a->z){
-            pai->right = a->z;
-            if(node->right != a->z)//Se possui filho para direita
-                pai->right = node->right;//O Pai pega os filhos
-            else if(node->right == a->z)
-                pai->right = a->z;//O pai aponta para NULL
-        }else{
-            while(node->left != a->z){//Enquanto for diferente de NULL
-                pai = node;//Pai recebe o que era filho
-                node = node->left;//Filho vai para o proximo
-            }
-            if(node->right != a->z)//Se possui filho para direita
-                pai->left = node->right;//O Pai pega os filhos
-            else if(node->right == a->z)
-                pai->left = a->z;//O pai aponta para NULL
-        }
-        aux->key = node->key;//node recebe o valor do menor no
-    }else{
-        if(node == a->raiz){
-            if(node->left == a->z && node->right == a->z){//Se não tiver Nenhum filho
-                no = a->z;//Vai apontar para o NULL
-                printf("\nExcluiu o ultimo elemento\n");
-            }else if(node->left == a->z && node->right != a->z)//Se tiver 1 filho
-                no = node->right;
-            else if(node->right == a->z && node->left != a->z)//1 filho
-                no = node->left;
-        }else{
-            while(no != node){//enquanto não chegar no nó
-                if(node->key > no->key){//Se o que quer excluir for maior que o nó
-                    if(no->right == node){//Se o da direita for igual ao nó
-                        if(node->left == a->z && node->right == a->z)//Se não tiver Nenhum filho
-                            no->right = a->z;//Vai apontar para o NULL
-                        else if(node->left == a->z && node->right != a->z)//Se tiver 1 filho
-                            no->right = node->right;
-                        else if(node->right == a->z && node->left != a->z)//1 filho
-                            no->right = node->left;
-                        break;//Parar o while
-                    }else
-                        no = no->right;//vai para o proximo
-                }else if(node->key < no->key){//Se o que quer excluir for menor que o nó
-                    if(no->left == node){
-                        if(node->left == a->z && node->right == a->z)//Se não tiver Nenhum filho
-                            no->left = a->z;//Vai apontar para o NULL
-                        else if(node->left == a->z && node->right != a->z)//Se tiver 1 filho
-                            no->left = node->right;
-                        else if(node->right == a->z && node->left != a->z)//1 filho
-                            no->left = node->left;
-                        break;//Parar o while
-                    }else
-                        no = no->left;//vai para o proximo
-                }
-            }
-        }
-    }
-    free(node);//Libera o node
-}
+    printf ("Achou %d \n", x->key);
 
 
+   x = busca(a, 55);
+   a->raiz = rotR(a, x);
+   printf ("Em largura depois de rotR(55)\n");
+   imprimeEmLargura(a);
+   printf("\n");
 
-#if 0
-void remover (ARVORE a, int key);
-void removerNo (ARVORE a, link node);
-void destroiArvore(ARVORE a);
-#endif
+   a->raiz = rotL(a, a->raiz);
+   printf ("Em largura depois de rotL(44)\n");
+   imprimeEmLargura(a);
+   printf("\n");
+
+   inserirNaRaiz(a, 46);
+   printf ("Insere na Raiz o elemento 46\n");
+   imprimeEmLargura(a);
+   printf("\n");
+
+   printf("Remove o 46 que possui 2 filhos\n");
+   remover (a, 46);
+   imprimeEmLargura(a);
+   printf("\n");
+
+   printf("Remove o 77 que nao possui filhos\n");
+   remover (a, 77);
+   imprimeEmLargura(a);
+   printf("\n");
+
+   printf("Remove o 66 que possui 1 filhos\n");
+   //remover (a, 66);
+   imprimeEmLargura(a);
+   printf("\n");
+   */
+   inserir(a, 55);
+   inserir(a, 44);
+   inserir(a, 33);
+   inserir(a, 66);
+   inserir(a, 77);
+   inserir(a, 45);
+   inserir(a, 64);
+    imprimeEmLargura(a);
+    printf("\n");
+    printf ("pre: ");
+  imprimePreOrdem(a);
+  printf ("\n");
+  printf ("pos: ");
+  imprimePosOrdem(a);
+  printf ("\n");
+  printf ("em: ");
+  imprimeEmOrdem(a);
+  printf ("\n");
+  printf ("largura: ");
+  imprimeEmLargura(a);
+  printf ("\n");
+   printf("Remove o 33 que possui 0 filhos\n");
+   remover (a, 33);
+   imprimeEmLargura(a);
+   printf("\n");
+   printf("Remove o 44 que possui 1 filhos\n");
+   remover (a, 44);
+   imprimeEmLargura(a);
+   printf("\n");
+   printf("Remove o 55 que possui 2 filhos\n");
+   remover (a, 55);
+   imprimeEmLargura(a);
+   printf("\n");
+    printf("Remove o 66 que possui 1 filhos\n");
+   remover (a, 66);
+   imprimeEmLargura(a);
+   printf("\n");
+   /*
+   printf("Remove o 64 que possui 2 filhos\n");
+   remover (a, 64);
+   imprimeEmLargura(a);
+   printf("\n");
+    printf("Remove o 77 que possui 2 filhos\n");
+   remover (a, 44);
+   imprimeEmLargura(a);
+   printf("\n");
+   printf("Remove o 66 que possui 1 filhos\n");
+   remover (a, 66);
+   imprimeEmLargura(a);
+   printf("\n");
+   printf("Remove o 33 que nao possui filhos\n");
+   remover (a, 33);
+   imprimeEmLargura(a);
+   printf("\n");
+    */
+
+   return 0;
+}
